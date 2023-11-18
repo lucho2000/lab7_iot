9880a5353ac927279728bc6a70f559160d8c6602
lucho2000 <luis.ramost@pucp.edu.pe>
2023-11-18 12:57:17 -0500

    commit final

---
+++ app/src/main/java/com/example/lab7_iot/DatosSalonActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab7_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosSalonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_salon);
+    }
+}
